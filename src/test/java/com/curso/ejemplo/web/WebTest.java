0de1f8e45a57923a6fd358388f1beb0dc8e5324d
Ruben Gomez <rubengomez78@gmail.com>
2018-04-17 18:33:53 +0200

    Added tests

---
+++ src/test/java/com/curso/ejemplo/web/WebTest.java
@@ -0,0 +1,27 @@
+package com.curso.ejemplo.web;
+
+import org.junit.Test;
+
+public class WebTest {
+
+	@Test
+	public void test1() {
+		assert(true);
+	}
+	@Test
+	public void test2() {
+		assert(true);
+	}
+
+	@Test
+	public void test3() {
+		assert(true);
+	}
+
+	@Test
+	public void test4() {
+		assert(true);
+	}
+
+
+}
