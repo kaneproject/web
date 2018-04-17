c8a4294894bb068de6d89fda54eb32e9dbfcc08f
Ruben Gomez <rubengomez78@gmail.com>
2018-04-17 19:30:08 +0200

    Added errors to sonar

---
+++ src/main/java/com/curso/ejemplo/web/ConClase.java
@@ -0,0 +1,27 @@
+package com.curso.ejemplo.web;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ConClase {
+	
+	public static void metodoConClase() {
+		String texto = null;
+        String clientes = "";
+        String texto2 = null;
+        String clientes2 = "";
+        List lista = new LinkedList<>();
+        BigDecimal b = new BigDecimal(12.1231d);
+        while(true) {
+        	System.out.println("infinito");
+        	try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+        }
+	}
+}
